788d08ae247d2af151ee39559300dcc615c72ca0
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-03-28 18:52:57 +0800

    alvistack/3.16.0
        git clean -xdf
        tar zcvf ../python-prettytable_3.16.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-prettytable.spec ../python-prettytable_3.16.0-1.spec
        cp ../python*-prettytable*3.16.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/jazzband-prettytable-3.16.0/
        rm -rf ../python*-prettytable*3.16.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='prettytable',
+    version='3.16.0',
+    description='A simple Python library for easily displaying tabular data in a visually appealing ASCII table format',
+    author_email='Luke Maurits <luke@maurits.id.au>',
+    maintainer='Hugo van Kemenade',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Text Processing',
+        'Typing :: Typed',
+    ],
+    install_requires=[
+        'wcwidth',
+    ],
+    extras_require={
+        'tests': [
+            'pytest',
+            'pytest-cov',
+            'pytest-lazy-fixtures',
+        ],
+    },
+    packages=[
+        'prettytable',
+    ],
+    package_dir={'': 'src'},
+)
